4861791bf3f473a7ba8e8e2d7ebbd64e05cbea4b
xplanet95 <vladis.box@yandex.ru>
2022-07-19 11:24:47 +0800

    command /start and echo

--- kn24_tg/main.py
+++ kn24_tg/main.py
@@ -1,12 +1,34 @@
-import telegram
+import logging  # для ведения логов
+import telegram as tg
+import telegram.ext
 import os
 import requests
 from dotenv import load_dotenv
 
+logging.basicConfig(  # для ведения логов (оф. документация)
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-def main():
-    pass
+load_dotenv()
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+# CHAT_ID = os.getenv('CHAT_ID')
+
+updater = tg.ext.Updater(token=TELEGRAM_TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
 
-if __name__ == "__main__":
-    main()
+def start(update: tg.Update, context: tg.ext.CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Введите код КН-24")
+
+
+def echo(update: tg.Update, context: tg.ext.CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+echo_handler = tg.ext.MessageHandler(tg.ext.Filters.text & (~tg.ext.Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+
+start_handler = tg.ext.CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+updater.start_polling()
